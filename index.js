d866b0a3ecbc30bc7e7355b6e4a0f86e299027e5
Lương Minh Anh <115328740+luongminhanh@users.noreply.github.com>
2023-03-13 00:18:47 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 const xClass = 'x';
 const oClass = 'cir';
 const cellElements = document.querySelectorAll('[data-cell]');
+const winningMessageTextElement = document.querySelector('.win-mess')
+const winningMessageElement = document.getElementById('win-mess')
 const board = document.getElementById('board');
-let oTurn 
+let oTurn
+const winning = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
 
-cellElements.forEach(cell => {
-    cell.addEventListener('click', handleClick, {once: true})
-})
+startGame()
 
+function startGame() {
+    oTurn = false;
+    cellElements.forEach(cell => {
+        cell.addEventListener('click', handleClick, { once: true })
+    })
+    setBoardHoverClass();
+}
 
 function handleClick(e) {
     //placeMark
@@ -15,11 +32,31 @@ function handleClick(e) {
     //Check for Draw
     //Switch Turns
     const cell = e.target;
-    console.log(cell);
     const currClass = oTurn ? oClass : xClass;
     placeMark(cell, currClass);
-    swapTurns();
-    setBoardHoverClass();
+    if (checkWin(currClass)) {
+        endGame(false);
+    } else if (isDraw()) {
+        endGame(true)
+    } else {
+        swapTurns();
+        setBoardHoverClass();
+    }
+}
+
+function endGame(draw) {
+    if (draw) {
+        winningMessageTextElement.innerHTML = "Draw!"
+    } else {
+        winningMessageTextElement.innerHTML = `${oTurn ? "O's" : "X's"} Wins!`
+    }
+    winningMessageElement.classList.add('show')
+}
+
+function isDraw() {
+    return [...cellElements].every(cell => {
+        return cell.classList.contains(xClass) || cell.classList.contains(oClass)
+    })
 }
 
 const placeMark = (cell, currClass) => {
@@ -37,3 +74,10 @@ function setBoardHoverClass() {
     else board.classList.add(xClass);
 }
 
+function checkWin(currClass) {
+    return winning.some(combination => {
+        return combination.every(index => {
+            return cellElements[index].classList.contains(currClass)
+        })
+    })
+}
